4a9d7cbd4ddf19c3f2f032142952b4d4ee97070a
vnyoon <vnyoon@163.com>
2023-08-31 16:58:57 +0800

    feat(concurrency): 控制最大次并发网络请求

---
+++ popular/01_concurrency-request/index.js
@@ -0,0 +1,55 @@
+const origiUrls = [];
+for (let i = 1; i <= 20; i++) {
+  origiUrls.push(`https://jsonplaceholder.typicode.com/todos/${i}`)
+};
+
+/**
+ * 
+ * @param {请求URL} urls 20
+ * @param {最大并发数} maxNum 3
+ */
+function concuRequest(urls, maxNum) {
+  return new Promise((resolve, reject) => {
+    if (urls.length === 0) return resolve([]);
+
+    const result = []; // 请求成功/失败结果
+    let index = 0; // 请求位置
+    let count = 0; // 请求数量
+    
+    async function reallyRequest() {
+      if (index >= urls.length) return;
+
+      const url = urls[index];
+      const indey = index++; // 记录请求位置，插入结果内容位置和请求位置保持一直，++是下一个要请求的位置
+
+      try {
+        const res = await fetch(url).then(res => res.json());
+
+        result[indey] = res;
+      } catch (error) {
+        
+        result[indey] = error;
+      } finally {
+        count++;
+
+        // 判断是否所有的请求都已完成
+        if (count === urls.length) {
+          resolve(result);
+        }
+
+        //无论成功/失败 发起下一次请求
+        reallyRequest();
+      }
+    };
+
+    // 请求urls数量小于最大并发数时，所以取二者最小值
+    const times = Math.min(urls.length, maxNum);
+    for (let i = 0; i < times; i++) {
+      reallyRequest();
+    }
+  })
+};
+
+concuRequest(origiUrls, 3).then(res => {
+  console.log("Result: ", res);
+});
