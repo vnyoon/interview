6aac62d8625302c98cccb8684f3796ef7ca75ee5
vnyoon <vnyoon@163.com>
2023-09-21 13:41:11 +0800

    feat(mouth): 计算星级评分函数

---
+++ mouth/01_star-rating/index.js
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * @param {星数} r 
+ * @returns 
+ */
+const rate = (r) => '★★★★★☆☆☆☆☆'.slice(5 - r, 10 - r);
+
+console.log(rate(0)); // ☆☆☆☆☆
+console.log(rate(1)); // ★☆☆☆☆
+console.log(rate(2)); // ★★☆☆☆
+console.log(rate(3)); // ★★★☆☆
+console.log(rate(4)); // ★★★★☆
+console.log(rate(5)); // ★★★★★
